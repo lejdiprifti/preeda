a3dc0ab3c9bd6d93fcb137165e38f83664250747
lprifti <priftilejdi@gmail.com>
2021-05-10 00:36:16 +0200

    adding finder window

--- src/components/MainBodyComponent.tsx
+++ src/components/MainBodyComponent.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import '../styles/MainBodyComponent.css';
+import { FinderWindowComponent } from "./FinderWindowComponent";
 
 export const MainBodyComponent:React.FC = (): JSX.Element => {
     return (
         <React.Fragment>
             <div id="mainBodyContainer">
-            <img src='address.png'/>
+                <img src='address.png'/>
             </div>
+            <FinderWindowComponent/>
         </React.Fragment>
     )
 }
