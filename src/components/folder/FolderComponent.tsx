cac6fdf1e0926c886ea79fad52fca2d8bb5d3a47
lprifti <priftilejdi@gmail.com>
2021-05-12 19:39:21 +0200

    added folder component and icons

--- src/components/folder/FolderComponent.tsx
+++ src/components/folder/FolderComponent.tsx
@@ -1,27 +1,25 @@
-import React, { CSSProperties, DragEventHandler, RefObject } from "react";
+import React, { ComponentProps, ComponentPropsWithRef, CSSProperties, DragEventHandler, RefObject } from "react";
 import Draggable from "react-draggable";
 import './FolderComponent.css';
 
 interface FolderComponentProps {
-    folderName: string
+    folderName: string,
+    onClick: () => void
 }
 
-interface FolderComponentState {
-
-}
-
-export class FolderComponent extends React.Component<FolderComponentProps, FolderComponentState> {
-
+export class FolderComponent extends React.Component<FolderComponentProps, {}> {
+    draggableRef: React.RefObject<HTMLDivElement>;
     constructor(props: FolderComponentProps) {
         super(props);
+        this.draggableRef = React.createRef();
     }
 
     render() {
         return (
-            <Draggable>
-                <div className="desktop-item">
+            <Draggable defaultPosition={{ x: Math.random() * 100, y: Math.random() * 100 }}>
+                <div ref={this.draggableRef} onDragStart={() => this.draggableRef.current?.classList.replace('draggedFolder', 'draggingFolder')} onDragEnd={() => this.draggableRef.current?.classList.replace('draggingFolder', 'draggedFolder')} className="desktop-item" onDoubleClick={() => { this.props.onClick() }}>
                     <div className="icon">
-                        <img id="folderImage" draggable="false" src='GenericFolder.png' alt='Folder' />
+                        {this.props.children}
                     </div>
                     <div className="title">
                         <span>{this.props.folderName}</span>
--- src/components/main-body/MainBodyComponent.tsx
+++ src/components/main-body/MainBodyComponent.tsx
@@ -8,9 +8,21 @@ export const MainBodyComponent: React.FC = (): JSX.Element => {
                 <img className="addressImg" src='address.png' alt="address" />
             </div>
             <div className="folderContainer">
-                <FolderComponent folderName='Drinks' />
-                <FolderComponent folderName='Food' />
-                <FolderComponent folderName='Tables' />
+                <FolderComponent folderName='Drinks' onClick={() => { console.log('clicked drinks') }}>
+                    <img className="folderImage" draggable="false" src='GenericFolder.png' alt='Folder' />
+                </FolderComponent>
+                <FolderComponent folderName='Food' onClick={() => { console.log('clicked drinks') }}>
+                    <img className="folderImage" draggable="false" src='GenericFolder.png' alt='Folder' />
+                </FolderComponent>
+                <FolderComponent folderName='Tables' onClick={() => { console.log('clicked drinks') }}>
+                    <img className="folderImage" draggable="false" src='GenericFolder.png' alt='Folder' />
+                </FolderComponent>
+                <FolderComponent folderName='Trash' onClick={() => { document.getElementById('root')!.innerHTML = '' }}>
+                    <img className="folderImage" draggable="false" src='TrashIcon.png' alt='Trash' />
+                </FolderComponent>
+                <FolderComponent folderName='Finder' onClick={() => { document.getElementById('root')!.innerHTML = '' }}>
+                    <img className="folderImage" draggable="false" src='FinderIcon.png' alt='Finder' />
+                </FolderComponent>
             </div>
         </div>
     )
