0e589e98763c310dff4c46b5d20d0c06543c0999
lprifti <priftilejdi@gmail.com>
2021-05-13 17:09:31 +0200

    fixed router bug

--- src/navigation/PreedaNavigation.tsx
+++ src/navigation/PreedaNavigation.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import { FooterComponent } from "../components/FooterComponent";
 import { HeaderComponent } from "../components/HeaderComponent";
 import { MainBodyComponent } from "../components/MainBodyComponent";
@@ -8,12 +8,12 @@ import { MenuComponent } from "../components/MenuComponent";
 export class PreedaNavigation extends React.Component {
     render() {
         return (
-            <BrowserRouter>
+            <HashRouter>
                 <HeaderComponent />
                 <Route exact path="/" ><MainBodyComponent /></Route>
                 <Route exact path="/menu"><MenuComponent /></Route>
                 <FooterComponent />
-            </BrowserRouter>
+            </HashRouter>
         )
     }
 }
