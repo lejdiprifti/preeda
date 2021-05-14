e7fe7ae583022fb3ca4441c815edb7f9f909c542
lprifti <priftilejdi@gmail.com>
2021-05-14 20:18:46 +0200

    added support for double tap on mobile

--- src/components/folder/FolderComponent.tsx
+++ src/components/folder/FolderComponent.tsx
@@ -13,9 +13,11 @@ const DESKTOP_ITEM_CLASSNAME = 'desktop-item';
 
 export class FolderComponent extends React.Component<FolderComponentProps, {}> {
     draggableRef: React.RefObject<HTMLDivElement>;
+    clickCount: number;
     constructor(props: FolderComponentProps) {
         super(props);
         this.draggableRef = React.createRef();
+        this.clickCount = 0;
     }
 
     handleDragStart() {
@@ -26,10 +28,25 @@ export class FolderComponent extends React.Component<FolderComponentProps, {}> {
         this.draggableRef.current!.style.zIndex = '1';
     }
 
+    handleClicks() {
+        this.clickCount++;
+        let singleClickTimer: NodeJS.Timeout;
+        const _ = this;
+        if (this.clickCount === 1) {
+            singleClickTimer = setTimeout(function () {
+                _.clickCount = 0;
+            }, 300);
+        } else if (this.clickCount === 2) {
+            clearTimeout(singleClickTimer!);
+            this.clickCount = 0;
+            this.props.onClick();
+        }
+    }
+
     render() {
         return (
             <Draggable defaultPosition={this.props.defaultPosition} onStart={() => this.handleDragStart()} defaultClassNameDragging='draggingFolder'>
-                <div ref={this.draggableRef} className={DESKTOP_ITEM_CLASSNAME} onDoubleClick={() => { this.props.onClick() }}>
+                <div ref={this.draggableRef} className={DESKTOP_ITEM_CLASSNAME} onTouchStart={() => { this.handleClicks() }} onDoubleClick={() => { this.props.onClick() }}>
                     <div className="icon">
                         {this.props.children}
                     </div>
